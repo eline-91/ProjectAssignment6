af9a341c100ff05249dd0593490f4029336311b0
eline-91 <elinevanelburg@hotmail.com>
2016-01-11 14:57:54 +0100

    Rathee_Elburg: select is added.

--- R/download_unzip.R
+++ R/download_unzip.R
@@ -1,4 +1,5 @@
 downloadFile_unzip <- function(url_file, dest_name, directory) {
+	# downloads files in the url to a given directory, under the given dest_name
 	setwd(directory)
 	download.file(url = url_file, destfile = dest_name, method = 'wget')
 	unzip(dest_name)
--- main.R
+++ main.R
@@ -2,14 +2,29 @@
 # Januray 11, 2016
 # Assignment Lesson 6
 
+# Call needed packages
+library(sp)
+library(rgdal)
+library(rgeos)
+
 # The two urls for the files
 url_places <- 'http://www.mapcruzin.com/download-shapefile/netherlands-places-shape.zip'
 url_railroad <- 'http://www.mapcruzin.com/download-shapefile/netherlands-railways-shape.zip'
 
 source('R/download_unzip.R')
 dest_directory <- 'data' # This folder should already exist in your project and be the folder where you want to put your data
-
 downloadFile_unzip(url_places, 'Netherlands_Places.zip', dest_directory)
 downloadFile_unzip(url_railroad, 'Netherlands_Railroad.zip', dest_directory)
 
+# Open the shapefiles just downloaded
+dsn_places = file.path("data","places.shp")
+dsn_railways = file.path("data","railways.shp")
+places <- readOGR(dsn_places, layer = ogrListLayers(dsn_places))
+railways <- readOGR(dsn_railways, layer = ogrListLayers(dsn_railways))
 
+# Select the industrial railways
+source('R/make_selection.R')
+type_railroad <- "industrial"
+subset_rails <- make_selection(railways, type, type_railroad)
+spplot(subset_rails)
+writer_names_df <- subset(writers_df, Name =="Jane")
