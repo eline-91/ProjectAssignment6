827f72a910a4b23b9efd23c15722adc5f517287c
eline-91 <elinevanelburg@hotmail.com>
2016-01-11 16:37:43 +0100

    Rathee_Elburg: commit of the whole assignment

--- main.R
+++ main.R
@@ -23,8 +23,27 @@ places <- readOGR(dsn_places, layer = ogrListLayers(dsn_places))
 railways <- readOGR(dsn_railways, layer = ogrListLayers(dsn_railways))
 
 # Select the industrial railways
-source('R/make_selection.R')
-type_railroad <- "industrial"
-subset_rails <- make_selection(railways, type, type_railroad)
-spplot(subset_rails)
-writer_names_df <- subset(writers_df, Name =="Jane")
+industrial_railways <- subset(railways, type == "industrial")
+
+# Transform to RD_New
+prj_string_RD <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +units=m +no_defs")
+industrial_railways_Tf <- spTransform(industrial_railways, prj_string_RD)
+places_Tf <- spTransform(places, prj_string_RD)
+
+# Buffer the "industrial" railways with a buffer of 1000 meters
+ind_railw_buffer <- gBuffer(industrial_railways_Tf, byid=TRUE, width=1000)
+
+# Intersect the buffer with the places dataset to get a TRUE/FALSE vector with which cities intersect and which not
+intersection <- gIntersects(ind_railw_buffer, places_Tf, byid=TRUE)
+
+# Subset the places dataset with the intersection vector
+city <- as.character(places$name[intersection == TRUE])
+population <- places$population[intersection == TRUE]
+
+# Plot the buffer, the point, and the name of the city
+point <- gIntersection(ind_railw_buffer, places_Tf)
+spplot(ind_railw_buffer, zcol="type", main = city, 
+			 sp.layout=list("sp.points", point, col="red", pch=19, cex=1.5))
+
+# Name of the city and population
+print(paste("Name of the city:", city, "Population:", population))
